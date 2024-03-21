dcc2328dae84f516c2bfbe3aeb21fd518ae65048
0SC4R24 <oscarviudez24@gmail.com>
2024-03-21 18:48:25 +0100

    Started Practica 1 Prog Web II Servidor

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+// app.get('/api', (req, res) => {
+//     res.send(JSON.stringify({ message: 'Hello World!' }));
+// });
+
+// app.get('/api/:name', (req, res) => {
+//     const name = req.params.name;
+//     const data = { message: `Hello ${name}!` };
+
+//     if (name === "") {
+//         return res.status(400).send({ error: 'Name is required' });
+//     }
+
+//     data.adult = (req.query.age > 17) ? true : false;
+
+//     res.send(JSON.stringify(data));
+// });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
